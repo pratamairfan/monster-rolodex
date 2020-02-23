ebc9b4e4493f20d8b335818baf8acc33d26ea91b
pratamairfan58 <pratamairfan58@gmail.com>
2020-02-24 00:18:17 +0700

    Updates

---
+++ precache-manifest.8358cb292a80b9573af2cd27677558de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18cf8cd5132afb0879a3413327f0cab6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "78ba8ca93c32379ba71f",
+    "url": "/monster-rolodex/static/css/main.fed64684.chunk.css"
+  },
+  {
+    "revision": "4c0b64b4272d6df9db5e",
+    "url": "/monster-rolodex/static/js/2.52a0770b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.52a0770b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ba8ca93c32379ba71f",
+    "url": "/monster-rolodex/static/js/main.49c5afe2.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
